3bb82338df8f8ebff1f055d97bf82cc3837e7a34
Taylor Klaus Cantalice Nobrega <taylorkcantalice@gmail.com>
2021-08-27 19:46:06 -0300

    menu

---
+++ menu.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+void Menu(){
+    cout << "\t\t Menu de Funcoes:\n
+             \t 1 - Cadastrar Produto\n
+             \t 2 - Buscar Produto\n
+             \t 3 - Relatorio de Estoque\n
+             \t 4 - "
+}
+
+int main(){
+
+
+    return 0;
+}
