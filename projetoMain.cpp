3d397c3d77860b6058177c62521290cbbd7ffb68
Rodrigo <64603683+Rodrigo-P-Nascimento@users.noreply.github.com>
2021-08-27 20:19:15 -0300

    up

--- projetoMain.cpp
+++ projetoMain.cpp
@@ -16,19 +16,43 @@ typedef struct {
     //Implementar o estoque
 }tProdutos;
 
-void PreencheArray(tProdutos prod);
-
+void PreencheArray(tProdutos *prod);
+void Menu();
 
 int main(){
 
     tProdutos produtos[TAM];
+    int opcao;
 
     PreencheArray(produtos);
 
+    while(1){
+        Menu();
+        cout << "Digite uma opcao: ";
+        cin >> opcao;
+        
+        if(opcao == 9){
+            break;
+        }
+
+        switch(opcao){
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            default:
+                cout << "Opcao " << opcao << " invalida." << endl;
+        }
+    }
     return 0;
 }
 
-void PreencheArray(tProdutos prod[]){ // Função para iniciar os produtos com valores padrões
+void PreencheArray(tProdutos *prod){ // Função para iniciar os produtos com valores padrões
 
     int i;
 
@@ -39,6 +63,12 @@ void PreencheArray(tProdutos prod[]){ // Função para iniciar os produtos com v
         prod[i].dataFabricacao = "vazio";
         prod[i].dataValidade = "vazio";
     }
+}
 
-
+void Menu(){
+    cout << "\t\t Menu de Funcoes:\n" <<
+            "\t 1 - Cadastrar Produto\n" <<
+            "\t 2 - Buscar Produto\n" <<
+            "\t 3 - Relatorio de Produtos\n" <<
+            "\t 9 - Sair" << endl;
 }
