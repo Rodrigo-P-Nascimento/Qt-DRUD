81308fadaccf960c668f35f870ae57abac4d4cd8
Taylor Klaus Cantalice Nobrega <taylorkcantalice@gmail.com>
2021-08-27 19:57:00 -0300

    menu

--- menu.cpp
+++ menu.cpp
@@ -1,15 +1,41 @@
 #include <iostream>
 
+using namespace std;
+
 void Menu(){
     cout << "\t\t Menu de Funcoes:\n
              \t 1 - Cadastrar Produto\n
              \t 2 - Buscar Produto\n
-             \t 3 - Relatorio de Estoque\n
-             \t 4 - "
+             \t 3 - Relatorio de Produtos\n
+             \t 9 - Sair\n"
 }
 
 int main(){
+    int opcao;
 
+    while(1){
+        Menu();
+        cout << "Digite uma opcao: " << endl;
+        cin >> opcao;
+        
+        if(opcao == 9){
+            break;
+        }
+
+        switch(opcao){
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            default:
+                cout << "Opcao " << opcao << " invalida."
+        }
+    }
 
     return 0;
 }
