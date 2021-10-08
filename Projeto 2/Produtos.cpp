bfa72f09ad40eddfe8837067b73ca002bd2bc9c7
LuizH-Queiroz <luizh.keiroz@gmail.com>
2021-10-08 15:01:05 -0300

    Arquivos .h e .cpp das classes

---
+++ Projeto 2/Produtos.cpp
@@ -0,0 +1,309 @@
+#include "Produtos.h"
+
+/********************* Class Remessa *********************/
+
+/******** CONSTRUTORES ********/
+Remessa::Remessa(){
+    dataFabricacao = dataValidade = "";
+}
+
+Remessa::Remessa(int dia_fab, int mes_fab, int ano_fab,
+                 int dia_val, int mes_val, int ano_val)
+{
+    setDataFabricacao(dia_fab, mes_fab, ano_fab);
+    setDataValidade(dia_val, mes_val, ano_val);
+}
+
+
+/******** MÉTODOS PRIVADOS ********/
+bool Remessa::diaValido(int dia, int mes) const{
+
+    if (dia <= 0){
+        return false;
+    }
+
+    switch (mes){
+        case 1:
+            if (dia > DiasMes::JANEIRO) return false;
+            break;
+        case 2:
+            if (dia > DiasMes::FEVEREIRO) return false;
+            break;
+        case 3:
+            if (dia > DiasMes::MARCO) return false;
+            break;
+        case 4:
+            if (dia > DiasMes::ABRIL) return false;
+            break;
+        case 5:
+            if (dia > DiasMes::MAIO) return false;
+            break;
+        case 6:
+            if (dia > DiasMes::JUNHO) return false;
+            break;
+        case 7:
+            if (dia > DiasMes::JULHO) return false;
+            break;
+        case 8:
+            if (dia > DiasMes::AGOSTO) return false;
+            break;
+        case 9:
+            if (dia > DiasMes::SETEMBRO) return false;
+            break;
+        case 10:
+            if (dia > DiasMes::OUTUBRO) return false;
+            break;
+        case 11:
+            if (dia > DiasMes::NOVEMBRO) return false;
+            break;
+        case 12:
+            if (dia > DiasMes::DEZEMBRO) return false;
+            break;
+        default: //Aqui é considerado um mês qualquer de 31 dias
+            if (dia <= 31){
+                return true;
+            }
+            else {
+                return false;
+            }
+    }
+
+
+    return true;
+}
+
+bool Remessa::mesValido(int mes) const{
+
+    if (mes <= 0 || mes > 12) return false;
+    else return true;
+}
+
+bool Remessa::anoValido(int ano) const{
+
+    if (ano < 0) return false;
+    else return true;
+}
+
+
+/******** MÉTODOS ********/
+void Remessa::setDataFabricacao(int dia, int mes, int ano){
+
+    dataFabricacao = "";
+
+    if (diaValido(dia, mes))
+    {
+        if (dia < 10) dataFabricacao += "0";
+
+        dataFabricacao += std::to_string(dia) + "/";
+    }
+    else
+    {
+        dataFabricacao += "00/";
+    }
+
+    if (mesValido(mes))
+    {
+        if (mes < 10) dataFabricacao += "0";
+
+        dataFabricacao += std::to_string(mes) + "/";
+    }
+    else {
+        dataFabricacao += "00/";
+    }
+
+    if (anoValido(ano))
+    {
+        dataFabricacao += std::to_string(ano);
+    }
+    else
+    {
+        dataFabricacao += "0000";
+    }
+}
+
+void Remessa::setDataValidade(int dia, int mes, int ano){
+
+    dataValidade = "";
+
+    if (diaValido(dia, mes))
+    {
+        if (dia < 10) dataValidade += "0";
+
+        dataValidade += std::to_string(dia) + "/";
+    }
+    else
+    {
+        dataValidade += "00/";
+    }
+
+    if (mesValido(mes))
+    {
+        if (mes < 10) dataValidade += "0";
+
+        dataValidade += std::to_string(mes) + "/";
+    }
+    else {
+        dataValidade += "00/";
+    }
+
+    if (anoValido(ano))
+    {
+        dataValidade += std::to_string(ano);
+    }
+    else
+    {
+        dataValidade += "0000";
+    }
+
+}
+
+
+
+/********************* Class RemessaUnidade *********************/
+
+/******** CONSTRUTORES ********/
+RemessaUnidade::RemessaUnidade()
+              : Remessa()
+{
+    quantProdutos = 0;
+}
+
+RemessaUnidade::RemessaUnidade(int n_prod, int dia_fab, int mes_fab, int ano_fab,
+                               int dia_val, int mes_val, int ano_val)
+              : Remessa(dia_fab, mes_fab, ano_fab, dia_val, mes_val, ano_val)
+{
+    if (n_prod > 0) quantProdutos = n_prod;
+    else quantProdutos = 0;
+}
+
+
+/******** MÉTODOS ********/
+void RemessaUnidade::setQuantProdutos(int quant){
+    
+    if (quant >= 0) quantProdutos = quant;
+}
+
+
+
+/********************* Class RemessaPeso *********************/
+
+/******** CONSTRUTORES ********/
+RemessaPeso::RemessaPeso()
+           : Remessa()
+{
+    peso = 0;
+}
+
+RemessaPeso::RemessaPeso(float peso_prod, int dia_fab, int mes_fab, int ano_fab,
+                        int dia_val, int mes_val, int ano_val)
+           : Remessa(dia_fab, mes_fab, ano_fab, dia_val, mes_val, ano_val)
+{
+    if (peso_prod > 0) peso = peso_prod;
+    else peso = 0;
+}
+
+
+/******** MÉTODOS ********/
+void RemessaPeso::setPeso(float peso){
+
+    if (peso >= 0) this->peso = peso;
+}
+
+
+
+/********************* Class Produto *********************/
+
+int Produto::n_produtos_instanciados = 0;
+
+/******** CONSTRUTORES ********/
+Produto::Produto(){
+    nome = "";
+    codigo = 0;
+    n_produtos_instanciados++;
+}
+
+Produto::Produto(std::string nome, int codigo, float preco){
+
+    this->nome = nome;
+
+    if (codigo > 0) this->codigo = codigo;
+    else this->codigo = 0;
+
+    if (preco >= 0) this->preco = preco;
+    else this->preco = 0;
+
+    n_produtos_instanciados++;
+}
+
+
+/******** DESTRUTOR ********/
+Produto::~Produto(){
+    n_produtos_instanciados--;
+}
+
+
+
+/******** MÉTODOS ********/
+void Produto::setNome(std::string nome){
+    this->nome = nome;
+}
+
+void Produto::setCodigo(int codigo){
+
+    if (codigo > 0) this->codigo = codigo;
+}
+
+void Produto::setPreco(float preco){
+
+    if (preco >= 0) this->preco = preco;
+}
+
+int Produto::getQuantProdutos(){
+    return n_produtos_instanciados;
+}
+
+
+
+/********************* Class ProdutoUnidade *********************/
+
+/******** CONSTRUTORES ********/
+ProdutoUnidade::ProdutoUnidade()
+              : Produto()
+{
+    remessa.clear();
+}
+
+ProdutoUnidade::ProdutoUnidade(std::string nome, int codigo, float preco, RemessaUnidade rem)
+              : Produto(nome, codigo, preco)
+{
+    remessa.push_back(rem);
+}
+
+/******** MÉTODOS ********/
+RemessaUnidade ProdutoUnidade::getRemessa(int indice) const{
+
+    if (indice >= 0 && indice < remessa.size()) return remessa[indice];
+}
+
+
+
+/********************* Class ProdutoPeso *********************/
+
+/******** CONSTRUTORES ********/
+ProdutoPeso::ProdutoPeso()
+           : Produto()
+{
+    remessa.clear();
+}
+
+ProdutoPeso::ProdutoPeso(std::string nome, int codigo, float preco, RemessaPeso rem)
+           : Produto(nome, codigo, preco)
+{
+    remessa.push_back(rem);
+}
+
+/******** MÉTODOS ********/
+RemessaPeso ProdutoPeso::getRemessa(int indice) const{
+
+    if (indice >= 0 && indice < remessa.size()) return remessa[indice];
+}
---
+++ Projeto 2/Produtos.h
@@ -0,0 +1,165 @@
+#ifndef BASE_V3
+#define BASE_V3
+
+#include <string>
+#include <vector>
+
+
+enum DiasMes 
+{ 
+    JANEIRO = 31,
+    FEVEREIRO = 28,
+    MARCO = 31,
+    ABRIL = 30,
+    MAIO = 31,
+    JUNHO = 30,
+    JULHO = 31,
+    AGOSTO = 31,
+    SETEMBRO = 30,
+    OUTUBRO = 31,
+    NOVEMBRO = 30,
+    DEZEMBRO = 31
+};
+
+/***************************************** REMESSAS ***************************************/
+class Remessa
+{
+    protected:
+        std::string dataFabricacao;
+        std::string dataValidade;
+
+        /******** MÉTODOS PROTECTED ********/
+        bool diaValido(int dia, int mes) const;
+        bool mesValido(int mes) const;
+        bool anoValido(int ano) const;
+
+    public:
+        /******** CONSTRUTORES ********/
+        Remessa();
+        Remessa(int dia_fab, int mes_fab, int ano_fab,
+                int dia_val, int mes_val, int ano_val);
+
+        /******** MÉTODOS ********/
+        std::string getDataFabricacao() const { return dataFabricacao; }
+        std::string getDataValidade() const { return dataValidade; }
+
+
+        void setQuantProdutos(int quant);
+        void setDataFabricacao(int dia, int mes, int ano);
+        void setDataValidade(int dia, int mes, int ano);
+};
+
+
+class RemessaUnidade : public Remessa
+{
+    private:
+        int quantProdutos;
+
+    public:
+        /******** CONSTRUTORES ********/
+        RemessaUnidade();
+        RemessaUnidade(int n_prod, int dia_fab, int mes_fab, int ano_fab,
+                int dia_val, int mes_val, int ano_val);
+
+        /******** MÉTODOS ********/
+        int getQuantProdutos() const { return quantProdutos; }
+
+        void setQuantProdutos(int quant);
+};
+
+
+class RemessaPeso : public Remessa
+{
+    private:
+        float peso;
+
+    public:
+        /******** CONSTRUTORES ********/
+        RemessaPeso();
+        RemessaPeso(float peso_prod, int dia_fab, int mes_fab, int ano_fab,
+                int dia_val, int mes_val, int ano_val);
+
+        /******** MÉTODOS ********/
+        float getPeso() const { return peso; }
+
+        void setPeso(float peso);
+};
+/******************************************************************************************/
+
+
+
+/************************************* PRODUTO ********************************************/
+class Produto
+{
+    private:
+        std::string nome;
+        int codigo;
+        float preco;
+
+        static int n_produtos_instanciados;
+
+    public:
+        /******** CONSTRUTORES ********/
+        Produto();
+        Produto(std::string nome, int codigo, float preco);
+
+        /******** DESTRUTOR ********/
+        ~Produto();
+
+        /******** MÉTODOS ********/
+        std::string getNome() const { return nome; }
+        int getCodigo() const { return codigo; }
+        float getPreco() const { return preco; }
+
+
+        void setNome(std::string nome);
+        void setCodigo(int codigo);
+        void setPreco(float preco);
+
+
+        void novaRemessa(Remessa rem);
+        void novaRemessa(int n_prod, int dia_fab, int mes_fab, int ano_fab,
+                         int dia_val, int mes_val, int ano_val);
+
+        static int getQuantProdutos();
+};
+
+
+class ProdutoUnidade : public Produto
+{
+    private:
+        std::vector<RemessaUnidade> remessa;
+
+    public:
+        /******** CONSTRUTORES ********/
+        ProdutoUnidade();
+        ProdutoUnidade(std::string nome, int codigo, float preco, RemessaUnidade rem);
+
+        /******** MÉTODOS ********/
+        RemessaUnidade getPrimeiraRemessa() const { return remessa.front(); }
+        RemessaUnidade getUltimaRemessa() const { return remessa.back(); }
+        RemessaUnidade getRemessa(int indice) const;
+        int getQuantRemessas() const { return remessa.size(); }
+};
+
+
+class ProdutoPeso : public Produto
+{
+    private:
+        std::vector<RemessaPeso> remessa;
+
+    public:
+        /******** CONSTRUTORES ********/
+        ProdutoPeso();
+        ProdutoPeso(std::string nome, int codigo, float preco, RemessaPeso rem);
+
+        /******** MÉTODOS ********/
+        RemessaPeso getPrimeiraRemessa() const { return remessa.front(); }
+        RemessaPeso getUltimaRemessa() const { return remessa.back(); }
+        RemessaPeso getRemessa(int indice) const;
+        int getQuantRemessas() const { return remessa.size(); }
+};
+/******************************************************************************************/
+
+
+#endif
