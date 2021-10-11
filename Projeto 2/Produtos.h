eaee5af5dbde44765e513b3cb6fdce0609088739
Taylor Klaus Cantalice Nobrega <taylorkcantalice@gmail.com>
2021-10-11 10:05:56 -0300

    Repository Update

--- Projeto 2/Produtos.h
+++ Projeto 2/Produtos.h
@@ -4,7 +4,6 @@
 #include <string>
 #include <vector>
 
-
 enum DiasMes 
 { 
     JANEIRO = 31,
--- Projeto 2/main.cpp
+++ Projeto 2/main.cpp
@@ -14,62 +14,62 @@ int LerOpcao(int comeco, int fim, int sair);
 void MenuMercearia(vector<Mercearia> &merc);
 
 int main(){
-    //classes de produtos
+    //Classes de Produtos
     ProdutoPeso PDpeso;
     ProdutoUnidade PDunidade;
     RemessaUnidade ReUNI;
     RemessaPeso RePES;
     vector<Mercearia> merc;
 
-    //Variavéis de suporte ao programa
+    //Variavéis de Suporte ao Programa
     int opcao, ativo=1;
     bool opInvalida;
 
     while(ativo){
         FazLinhas(LINHAS);
-        cout << "Em qual das seções deseja trabalhar?" << endl;
+        cout << "Em qual das Secoes deseja trabalhar?" << endl;
         FazLinhas(LINHAS);
-        cout << "[1] Mercearia" << endl;
-        cout << "[2] Frios/Acougue" << endl;
-        cout << "[3] Padaria" << endl;
-        cout << "[4] Bebidas" << endl;
-        cout << "[5] Limpeza" << endl;
-        cout << "[-1] Sair do Programa" << endl;
+        cout << "\t[1] Mercearia" << endl;
+        cout << "\t[2] Frios/Acougue" << endl;
+        cout << "\t[3] Padaria" << endl;
+        cout << "\t[4] Bebidas" << endl;
+        cout << "\t[5] Limpeza" << endl;
+        cout << "\t[-1] Sair do Programa" << endl;
 
-
+        cout << "\nDigite uma opcao: ";
         opcao = LerOpcao(1, 5, -1);
     
         switch (opcao){
             case 1:
                 MenuMercearia(merc);
                 break;
+            case 2:
+                //MenuFrios_Acougue();
+                break;
+            case 3:
+                //MenuPadaria();
+                break;
+            case 4:
+                //MenuBebidas();
+                break;
+            case 5:
+                //MenuLimpeza();
+                break;
             case -1:
                 ativo = 0;
                 break;
         }
-    } // Ende while(ativo)
+    }// End While(ativo)
     
-
     return 0;
-}
+}// End main()
 
 void FazLinhas(int numLinhas){
     for(int i=1; i <= numLinhas; i++){
         if(i == numLinhas) cout << "-" << endl;
         else cout << "-";
     }
-}
-
-void Menu(){
-    FazLinhas(LINHAS);
-    cout << "[1]    > Cadastrar novo Produto." << endl;
-    cout << "[2]    > Adicionar uma Remessa." << endl;
-    cout << "[3]    > Remover um Produto." << endl;
-    cout << "[-1]   > Voltar para Menu anterior." << endl;
-    FazLinhas(LINHAS);
-
-    cout << "Digite uma opcao: ";
-}
+}// End FazLinhas()
 
 int LerOpcao(int comeco, int fim, int sair){
     int opcao;
@@ -84,14 +84,26 @@ int LerOpcao(int comeco, int fim, int sair){
         }
         else {
             cout << "Opcao invalida! Tente novamente!" << endl;
+            cout << "\nDigite uma opcao: ";
         }
-    }
-}
+    }// End While(opcaoInvalida)
+}// End LerOpcao()
+
+void Menu(){
+    FazLinhas(LINHAS);
+    cout << "[1] Cadastrar novo Produto" << endl;
+    cout << "[2] Adicionar uma Remessa" << endl;
+    cout << "[3] Remover um Produto" << endl;
+    cout << "[-1] Voltar para Menu Anterior" << endl;
+    FazLinhas(LINHAS);
+
+    cout << "\nDigite uma opcao: ";
+}// End Menu()
 
 void MenuMercearia(vector<Mercearia> &merc){
     int opcao;
 
-    
+    FazLinhas(LINHAS);
     cout << "Bem-vindo a secao de Mercearia!" << endl;
     while(true)
     {
@@ -110,5 +122,6 @@ void MenuMercearia(vector<Mercearia> &merc){
             case -1:
                 return;
         }
+        cout << endl;
     }// End while(ativo)
-}
+}// End MenuMercearia()
