d4f69d592edbac897b1d9dc8a1ce1c794d43e4ac
Taylor Klaus Cantalice Nobrega <taylorkcantalice@gmail.com>
2021-11-04 18:24:07 -0300

    Repository Update

--- Projeto 3/Projeto/cadastromercearia.cpp
+++ Projeto 3/Projeto/cadastromercearia.cpp
@@ -20,7 +20,7 @@ void CadastroMercearia::on_BTN_SALVAR_MERC_clicked(){
     QString unidade = ui->SPIN2_MERC->text();
     //Informações do Fornecedor
     QString nomeFornecedor = ui->LINE_NOME_FORNECEDOR->text();
-    QString pais = ui->LINE_PAIS->text();
+    QString pais = ui->COMBOBOX_PAIS->currentText();
     QString estado = ui->LINE_ESTADO->text();
     QString cidade = ui->LINE_CIDADE->text();
 
@@ -43,7 +43,6 @@ void CadastroMercearia::on_BTN_CANCELAR_MERC_clicked(){
     ui->SPIN2_MERC->clear();
 
     ui->LINE_NOME_FORNECEDOR->clear();
-    ui->LINE_PAIS->clear();
     ui->LINE_ESTADO->clear();
     ui->LINE_CIDADE->clear();
 
